3bb2f7c76af06ae76f05f840d09dd579e77c02d3
Towelman-public <odagiritomouya@outlook.jp>
2022-07-12 20:28:13 +0900

    fix around DB

--- repository/connection.tsx
+++ repository/connection.tsx
@@ -1,19 +1,24 @@
 import * as mysql from "promise-mysql";
 
-async function connection() {
-  const connection = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER_NAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME,
-    multipleStatements: true,
-  });
+let m_connection: mysql.Connection | null = null;
 
-  return connection;
+async function connection() {
+  if (m_connection == null) {
+    m_connection = await mysql.createConnection({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER_NAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB_NAME,
+      multipleStatements: true,
+      timezone: "jst",
+    });
+  }
+
+  return m_connection;
 }
 
-async function sqlQuery(sql: string): Promise<any> {
-  return (await connection()).query(sql);
+async function sqlQuery(sql: string, ...value: any): Promise<any> {
+  return (await connection()).query(sql, ...value);
 }
 
 export default sqlQuery;
--- test-db/init.sql
+++ test-db/init.sql
@@ -2,7 +2,6 @@ USE simple-memo;
 
 CREATE TABLE memo_list_owner(
 	memo_list_owner_id INT PRIMARY KEY AUTO_INCREMENT,
-	memo_list_owner_token VARCHAR(100) UNIQUE,
 	owner_validity_period  DATETIME NOT NULL,
 	is_deleted TINYINT(1) DEFAULT 0
 );
@@ -10,7 +9,8 @@ CREATE TABLE memo_list_owner(
 CREATE TABLE memo(
 	memo_id INT PRIMARY KEY AUTO_INCREMENT,
 	memo_list_owner_id INT NOT NULL,
-	memo_text DATETIME NOT NULL,
+	memo_title VARCHAR(50) NOT NULL,
+	memo_content VARCHAR(2000) NOT NULL,
 
     CONSTRAINT fk_memo_list_owner_id_to_memo
 	    FOREIGN KEY (memo_list_owner_id) 
@@ -50,7 +50,6 @@ USE test;
 
 CREATE TABLE memo_list_owner(
 	memo_list_owner_id INT PRIMARY KEY AUTO_INCREMENT,
-	memo_list_owner_token VARCHAR(100) UNIQUE,
 	owner_validity_period  DATETIME NOT NULL,
 	is_deleted TINYINT(1) DEFAULT 0
 );
@@ -58,7 +57,8 @@ CREATE TABLE memo_list_owner(
 CREATE TABLE memo(
 	memo_id INT PRIMARY KEY AUTO_INCREMENT,
 	memo_list_owner_id INT NOT NULL,
-	memo_text DATETIME NOT NULL,
+	memo_title VARCHAR(50) NOT NULL,
+	memo_content VARCHAR(2000) NOT NULL,
 
     CONSTRAINT fk_memo_list_owner_id_to_memo
 	    FOREIGN KEY (memo_list_owner_id) 
